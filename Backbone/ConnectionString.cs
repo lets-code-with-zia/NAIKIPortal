de7f1d6976ba25a0b07c7c1c5dac65ccc9ee6cd9
zia <letscode786@gmail.com>
2022-03-07 18:29:44 +0500

    added database and utility files

---
+++ Backbone/ConnectionString.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Configuration;
+
+namespace NAIKI.Backbone
+{
+    public class ConnectionString
+    {
+
+        private static String _Naiki = String.Empty;
+        public static String SerBeeConnectionString
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(_Naiki))
+                {
+                    string userName = WebConfigurationManager.AppSettings["DB"];
+
+                    _Naiki = userName;
+                }
+
+                var result = _Naiki;
+
+
+
+                return result;
+
+            }
+        }
+
+    }
+}
