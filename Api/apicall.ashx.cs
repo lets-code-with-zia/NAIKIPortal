e629cc9232b17e542fc0682ba33cdee871e97af2
sohaib <soaibrafique786@gmail.com>
2022-03-17 19:33:34 +0500

    mark as done api done

--- Api/apicall.ashx.cs
+++ Api/apicall.ashx.cs
@@ -240,6 +240,25 @@ namespace NAIKI.Api
                 if (methodName.ToLower() == "MarkAsDoneJob".ToLower())
                 {
 
+                    int userId = 0;
+                    int.TryParse(context.Request.QueryString["uID"], out userId);
+                    if (userId == 0)
+                    {
+                        throw new Exception("Invalid or no user id found");
+                    }
+                    int jobTypeId = 0;
+                    int.TryParse(context.Request.QueryString["jID"], out jobTypeId);
+                    if (jobTypeId == 0)
+                    {
+                        throw new Exception("Invalid or no job type form");
+                    }
+                    JobFactory oFactory = new JobFactory();
+
+                    oFactory.MarkAsDoneJob(userId, jobTypeId);
+                    context.Response.ContentType = "application/json";
+                    context.Response.ContentEncoding = System.Text.Encoding.UTF8;
+
+                    context.Response.Write(jsonSerializer.Serialize(new Dictionary<string, dynamic>() { { "IsError", false }, { "ErrorMessage", "" } }));
                 }
                 #endregion
 
--- Services/JobFactory.cs
+++ Services/JobFactory.cs
@@ -42,5 +42,59 @@ namespace NAIKI.Services
                 throw new Exception(ex.Message);
             }
         }
+
+        public void MarkAsDoneJob(int userId , int jobId)
+        {
+            try
+            {
+                using (DataClasses1DataContext eDataBase = new DataClasses1DataContext(ConnectionString.NAIKIConnectionString))
+                {
+                    Job eJob = new Job();
+                    eJob = eDataBase.Jobs.Where(eJData => eJData.Id == jobId && eJData.IsDeleted == false && eJData.IsActive == true).FirstOrDefault();
+                    if (eJob == null)
+                    {
+                        throw new Exception("Invalid or no job found");
+                    }
+                    eJob.StatusId = 3;
+                    var eLib = eJob.UserJobLibraries.Where(eLibData => eLibData.IsActive == true && eLibData.IsDeleted == false && eLibData.UserId == userId).FirstOrDefault();
+                    if (eLib == null)
+                    {
+                        throw new Exception("No or invalid entry Found");
+
+                    }
+                    eLib.StatusId = 3;
+                    eLib.CompletedOn = DateTime.Now;
+
+                    eDataBase.SubmitChanges();
+
+                    var jobCount = eDataBase.UserJobLibraries.Where(eJCount => eJCount.UserId == userId && eJCount.IsActive == true && eJCount.IsDeleted == false && eJCount.StatusId == 3).Count();
+                    var eBadge = eDataBase.RewardBadges.Where(eBData => jobCount ==  eBData.MileStoneCount && eBData.IsActive == true).FirstOrDefault();
+
+                    if (eBadge != null)
+                    {
+                        var record = eDataBase.UserRewardBadges.Where(eRecord => eRecord.UserId == userId && eRecord.RewardBadgeId == eBadge.Id 
+                        && eRecord.IsActive == true && eRecord.IsDeleted == false).FirstOrDefault();
+                        if (record == null)
+                        {
+                            UserRewardBadge eReward = new UserRewardBadge();
+                            eReward.UserId = userId;
+                            eReward.RewardBadgeId = eBadge.Id;
+                            eReward.EarnedOn = DateTime.Now;
+                            eReward.IsActive = true;
+                            eReward.IsDeleted = false;
+                            eDataBase.UserRewardBadges.InsertOnSubmit(eReward);
+                            eDataBase.SubmitChanges();
+                        }
+                       
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
     }
 }
